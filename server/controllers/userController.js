03c2ae62a71d159ee2f99f2a3bc912eff5fb3f59
jonav01 <shreyonjoneskol@gmail.com>
2023-03-02 16:57:19 +0530

    Authentication routes configured and password encrypted

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,27 @@
-const registerUser = async (req, res) => {
-  const { name, email, phone, password } = req.body;
-  res.json({
-    name,
-    email,
-  });
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
-module.exports={registerUser}
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, phone, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  } else {
+    const user = await User.create({
+      name,
+      email,
+      phone,
+      password,
+    });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      phone: user.phone,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  }
+});
+
+module.exports = { registerUser };
---
+++ server/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+const User = mongoose.model("users", userSchema);
+module.exports = User;
