cb13dfde9179cc9359ac1151c01aaecead295b43
jonav01 <shreyonjoneskol@gmail.com>
2023-03-09 20:25:12 +0530

    Removed production config from server.js

--- server/server.js
+++ server/server.js
@@ -14,20 +14,22 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
-
-// --------------deployment-----------------
-__dirname=path.resolve()
-if(process.env.NODE_ENV === "production"){
-app.use(express.static(path.join(__dirname,'/client/dist')))
-
-app.get('*' , (req,res) => {
-  res.sendFile(path.resolve(__dirname , 'client' , 'dist' , 'index.html'))
+app.get('/' , (req,res) => {
+  res.status(200).send('App is running')
 })
-}else{
-  app.get('/' , (req,res) => {
-    res.send("API is running..")
-  })
-}
+// --------------deployment-----------------
+// __dirname=path.resolve()
+// if(process.env.NODE_ENV === "production"){
+// app.use(express.static(path.join(__dirname,'/client/dist')))
+
+// app.get('*' , (req,res) => {
+//   res.sendFile(path.resolve(__dirname , 'client' , 'dist' , 'index.html'))
+// })
+// }else{
+//   app.get('/' , (req,res) => {
+//   res.send("API is running..")
+//   })
+// }
 // --------------deployment-----------------
 
 app.use(notFound)
